ecb695dba626fabeaec3166b0ed144296fd8edcd
BENONI ESCKINDER <you@example.com>
2024-04-18 09:46:09 +0300

    Pycodestyle fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -22,7 +22,8 @@ def canUnlockAll(boxes):
 
     if boxes:
         for key in boxes[0]:
-            if key < len(boxes): opened_boxes.append(boxes[key])
+            if key < len(boxes):
+                opened_boxes.append(boxes[key])
 
     closed_boxes = [box for box in boxes if box not in opened_boxes]
 
