a7ca3c9fdf7c2defe042e80d8ef25d91e270edd0
BENONI ESCKINDER <besckinder@gmail.com>
2024-04-18 08:51:31 +0300

    Lockboxes algorithm complete

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+''' Can I open all the boxes '''
+
+from queue import Queue
+
+
+def canUnlockAll(boxes):
+    ''' Returns a boolean determining if you can open all boxes or not '''
+    keys = set()
+    opened_boxes = [boxes[0]]
+
+    def get_keys(boxes):
+        """ Gets all the keys for the boxes recursively """
+        for item in boxes:
+            if isinstance(item, list):
+                get_keys(item)
+            else:
+                keys.add(item)
+
+        return keys
+
+    for key in boxes[0]:
+        opened_boxes.append(boxes[key])
+
+    closed_boxes = [box for box in boxes if box not in opened_boxes]
+
+    for box in boxes:
+        if box in opened_boxes:
+            continue
+
+        keys = get_keys(opened_boxes)
+        if boxes.index(box) in keys:
+            opened_boxes.append(box)
+            closed_boxes.remove(box)
+        else:
+            return False
+    return True
