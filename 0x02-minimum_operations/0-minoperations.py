d992f032a0c411245c736477cca238b627752860
BENONI ESCKINDER <besckinder@gmail.com>
2024-04-23 14:07:58 +0300

    Making it executable

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,14 +4,13 @@
     minOperations '''
 
 
-def prime(n):
+def is_prime(n):
     ''' Find out if a number is prime '''
     for i in range(2,  int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
-''' Big man ting '''
 
 def minOperations(n):
     ''' Whatever you day big dog '''
