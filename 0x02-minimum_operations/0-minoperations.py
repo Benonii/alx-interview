c2aeabf7dd54c0eaf5609210b6ea212101e2aac3
BENONI ESCKINDER <besckinder@gmail.com>
2024-04-23 14:07:11 +0300

    Making it executable

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,12 +4,34 @@
     minOperations '''
 
 
-def is_prime(n):
+def prime(n):
     ''' Find out if a number is prime '''
     for i in range(2,  int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
+
+''' Big man ting '''
+
+def minOperations(n):
+    ''' Whatever you day big dog '''
+    if not isinstance(n, int) or n <= 1:
+        return 0
+
+    # Function to find prime factors using the Sieve of Eratosthenes
+    def prime_factors(n):
+        ''' We prime baby '''
+        factors = []
+        for i in range(2, int(n**0.5) + 1):
+            while n % i == 0:
+                factors.append(i)
+                n //= i
+        if n > 1:
+            factors.append(n)
+        return factors
+
+    factors = prime_factors(n)
+    return sum(factors)
 
 
 def find_prime_factors(n):
@@ -35,7 +57,7 @@ def minOperations(n):
     if not isinstance(n, int) or n <= 1:
         return 0
     primes = find_prime_factors(n)
-    print(primes)
+    # print(primes)
 
     min_ops = factorize_add_primes(n, primes, 0)
 
