567cb191375aad30e12b93ca1926a4b4b27cfd47
BENONI ESCKINDER <you@example.com>
2024-04-23 08:37:42 +0300

    Don't go further if n <= 1 check

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -37,7 +37,7 @@ def add_prime_factors(n, primes, Sum):
 def minOperations(n):
     ''' Function to determine the miminum operations required to print a
         character n times wit copyAll and paste as the only operations. '''
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n <= 1:
         return 0
     primes = find_prime_factors(n)
 
