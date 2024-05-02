b9908b629bd0a18949d6b30aabe33e5cefd328f9
BENONI ESCKINDER <you@example.com>
2024-05-02 15:45:19 +0300

    Fixed stdin reading

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,39 +6,42 @@ import sys
 import re
 import signal
 
-def signal_handler(signal):
+
+total_size = 0
+statuses = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def signal_handler(signal, form):
     ''' Handler for SIGINT '''
-    print_stats()
+    print_stats(total_size, statuses)
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 def print_stats(total_size, statuses):
     ''' Prints number of status codes and total file size '''
     print(f"File size: {total_size}")
-            for key, value in sorted(statuses.items()):
-                if statuses[key] == 0:
-                    continue
-                else:
-                    print(f'{key}: {value}')
+    for key, value in sorted(statuses.items()):
+        if statuses[key] == 0:
+            continue
+        else:
+            print(f'{key}: {value}')
 
+
+ip_addr_pattern = r'[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.'\
+                    + r'[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]'
+date_pattern = r'[12][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9] '\
+                + r'[0-2][0-9]:[0-6][0-9]:[0-6][0-9].\d{6}'
+string_pattern = r'"GET /projects/260 HTTP/1.1"'
+status_size_pattern = r'(200|301|400|401|403|404|405|500) \d{1,4}'
+final_pattern = f'{ip_addr_pattern} - \\[{date_pattern}\\]'\
+                + f' {string_pattern} {status_size_pattern}'
+count = 0
+# logs = sys.stdin.readlines()
 try:
-    ip_addr_pattern = r'[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.'\
-                      + r'[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]'
-    date_pattern = r'[12][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9] '\
-                   + r'[0-2][0-9]:[0-6][0-9]:[0-6][0-9].\d{6}'
-    string_pattern = r'"GET /projects/260 HTTP/1.1"'
-    status_size_pattern = r'(200|301|400|401|403|404|405|500) \d{1,4}'
-    final_pattern = f'{ip_addr_pattern} - \\[{date_pattern}\\]'\
-                    + f' {string_pattern} {status_size_pattern}'
-    total_size = 0
-    statuses = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    count = 0
-    logs = sys.stdin.readlines()
-
-    # print(final_pattern)
-    for log in logs:
-        # print(log)
+    for log in sys.stdin:
         if re.match(final_pattern, log) is not None:
             status_size = re.search(status_size_pattern, log)
             status_size = str(status_size.group())
@@ -55,7 +58,9 @@ try:
         count += 1
         total_size += size
 
-        if count % 10 == 0 or KeyboardInterrupt:
-           print_stats()
+        if count % 10 == 0:
+            print_stats(total_size, statuses)
 except KeyboardInterrupt:
+    pass
+finally:
     print_stats()
