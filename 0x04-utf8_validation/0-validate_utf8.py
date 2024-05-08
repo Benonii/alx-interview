66762c8e8b4b7ef2c911a64804deecb0b083e5de
BENONI ESCKINDER <you@example.com>
2024-05-09 00:55:38 +0300

    First commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+Contains the function validUTF8()
+"""
+
+
+def validUTF8(data):
+    ''' Make sure data is utf-8 encoded(encodable) by using bitwise operations
+    '''
+
+    def calculate_bytes(num):
+        ''' calculates how many bytes are used to represent a character in
+            utf-8 '''
+        mask = 0b10000000
+
+        bytes = 0
+        while num & mask:
+            mask >>= 1
+            bytes += 1
+        return bytes
+
+    i = 0
+    while i < len(data):
+        bytes = calculate_bytes(data[i])
+
+        if bytes > 4 or bytes == 1:
+            return False
+
+        if bytes == 0:
+            i += 1
+            continue
+
+        for j in range(1, bytes):
+            ''' if calculate_bytes() returns 1, the number in binary
+                starts with '10', making it a continuation bit '''
+            i += 1
+            is_continuation_bit = calculate_bytes(data[i])
+            if is_continuation_bit != 1:
+                return False
+    return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+# Using bitwise operations to validate utf data given in the form of a lsit of integers
