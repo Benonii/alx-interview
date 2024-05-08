3af19244358555e853334445a462113eccef052d
BENONI ESCKINDER <you@example.com>
2024-05-09 01:37:02 +0300

    Adding another incrementer

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -27,7 +27,10 @@ def validUTF8(data):
 
         if bytes > 4 or bytes == 1 or k >= len(data):
             return False
- 
+
+        if bytes == 0:
+            i += 1
+
         for j in range(1, bytes):
             ''' if calculate_bytes() returns 1, the number in binary
                 starts with '10', making it a continuation bit '''
