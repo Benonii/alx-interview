c1ab99041f5ae77a353cf438215ce518733bf752
BENONI ESCKINDER <besckinder@gmail.com>
2024-06-04 12:06:49 +0300

    Dynamically solving the problem with memoiztion

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+''' This module contains the function makeChange() '''
+
+
+def makeChange(coins, total):
+    ''' Returns the minimum number of coins needed to make change '''
+
+    if total <= 0:
+        return 0
+
+    memo = [0]
+    for i in range(1, total + 1):
+        memo.append(total + 1)
+
+    for i in range(1, len(memo)):
+        for coin in coins:
+            if coin <= i:
+                memo[i] = min(memo[i - coin] + 1, memo[i])
+        # print(f"Memo on iteration {i}: {memo}")
+    if memo[total] > total:
+        return -1
+
+    return memo[total]
